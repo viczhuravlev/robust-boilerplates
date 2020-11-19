53fae0e103806d427d94f29d3afeea3768688699
viczhuravlev <vzhuravlev15@gmail.com>
2020-11-19 23:49:15 +0300

    chore: update babelrc

---
+++ frontend/src/index.tsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import { render } from 'react-dom';
+
+// import App from './App';
+
+// render(<App />, document.getElementById('app'));
+
+const test = {
+  asdasd: 'asdasd',
+  asdasd1: 'asdasd',
+  asdasd2: 'asdasd',
+  asdasd3: 'asdasd',
+};
