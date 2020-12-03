cc9ecd04227806aec6dc7012a919f0b4381061dd
viczhuravlev <vzhuravlev15@gmail.com>
2020-12-03 22:46:13 +0300

    feat: add config for tools and support env

---
+++ react/tools/config/env.js
@@ -0,0 +1,63 @@
+const path = require('path');
+
+const { rootPath } = require('./paths');
+
+const result = require('dotenv').config({ path: path.resolve(rootPath, '.env') });
+
+if (result.error) {
+  throw new Error(`[Couldn't find .env file]: ${result.error}️`);
+}
+
+const NODE_ENV = process.env.NODE_ENV;
+
+/**
+ * Application-only variables
+ */
+const appEnv = {
+  NODE_ENV,
+
+  SERVER_URL: process.env.SERVER_URL,
+};
+
+/**
+ * Webpack-only variables
+ */
+const webpackEnv = {
+  NODE_ENV,
+  PUBLIC_URL: process.env.PUBLIC_URL,
+
+  DEV_SERVER_HOST: process.env.DEV_SERVER_HOST,
+  DEV_SERVER_PORT: process.env.DEV_SERVER_PORT,
+
+  BUNDLE_ANALYZER: process.env.BUNDLE_ANALYZER,
+  BUNDLE_ANALYZER_HOST: process.env.BUNDLE_ANALYZER_HOST,
+  BUNDLE_ANALYZER_PORT: process.env.BUNDLE_ANALYZER_PORT,
+};
+
+/**
+ * Check all env
+ */
+function checkVariablesFull(env) {
+  const emptyKeys = [];
+
+  Object.keys(env).forEach((key) => {
+    if (!env[key]) {
+      emptyKeys.push(key);
+    }
+  });
+
+  if (emptyKeys.length > 0) {
+    throw new Error(`${emptyKeys.join(', ')} is required.`);
+  }
+}
+
+checkVariablesFull({
+  ...appEnv,
+  ...webpackEnv,
+});
+
+module.exports = {
+  appEnv,
+  webpackEnv,
+  isProduction: NODE_ENV === 'production',
+};
---
+++ react/tools/config/paths.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+  rootPath: path.resolve(__dirname, '../../'),
+  sourcePath: path.resolve(__dirname, '../../src'),
+  buildPath: path.resolve(__dirname, '../../build'),
+  templatesPath: path.resolve(__dirname, '../../templates'),
+};
