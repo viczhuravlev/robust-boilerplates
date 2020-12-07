9d6bd1d9b32feba95b58e35aeb2a8158f0d89a8f
viczhuravlev <vzhuravlev15@gmail.com>
2020-12-08 00:17:22 +0300

    feat: add favicon

--- react/tools/webpack/plugins/html.js
+++ react/tools/webpack/plugins/html.js
@@ -4,7 +4,7 @@
 const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
 
-const { templatesPath } = require('../../config/paths');
+const { sourcePath, templatesPath } = require('../../config/paths');
 const { isProduction, webpackEnv } = require('../../config/env');
 
 const minifyConfig = {
@@ -24,5 +24,6 @@ const minifyConfig = {
 module.exports = new HtmlPlugin({
   title: 'Robust React Application',
   minify: isProduction ? minifyConfig : false,
+  favicon: path.resolve(sourcePath, './static/images/favicon.png'),
   template: path.resolve(templatesPath, `./index.${webpackEnv.NODE_ENV}.html`),
 });
