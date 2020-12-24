85f43d05b810f9bdf5965680681897defc863329
viczhuravlev <vzhuravlev15@gmail.com>
2020-12-25 00:51:16 +0300

    chore: move components to core

---
+++ react/src/core/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -0,0 +1,35 @@
+import React, { useCallback, useState } from 'react';
+
+import * as Theme from '@/utils/theme';
+import { ThemeMode } from '@/utils/theme';
+
+import * as T from './ThemeSwitcher.types';
+import * as S from './ThemeSwitcher.styles';
+
+function ThemeSwitcher(props: T.ThemeSwitcherProps) {
+  const { className } = props;
+
+  const [theme, setTheme] = useState<Theme.ThemeMode>(Theme.getCurrentThemeMode());
+
+  const onSwitchOnThemeClick = useCallback(() => {
+    const currentTheme = Theme.switchOnTheme();
+
+    setTheme(currentTheme);
+  }, [theme]);
+
+  const isLight = theme === ThemeMode.light;
+
+  return (
+    <S.SwitchButton
+      role="img"
+      onClick={onSwitchOnThemeClick}
+      className={className}
+      data-moon={isLight ? '🌑' : '🌕'}
+      aria-label="Moon"
+    >
+      {isLight ? '🌓' : '🌗'}
+    </S.SwitchButton>
+  );
+}
+
+export default ThemeSwitcher;
